6ed01af590c056689a66e808e546ec29a7919ba1
Fasamii <fasami0512@gmail.com>
2025-04-07 18:09:59 +0200

    code indentation change

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function clockInit(secID,minID,hrsID){
         min.textContent = date.getMinutes().toString().padStart(2,'0');
         sec.textContent = date.getSeconds().toString().padStart(2,'0');
 
-			document.title = date.getHours().toString().padStart(2,'0') + ":"
-			+ date.getMinutes().toString().padStart(2,'0') + ":"
-			+ date.getSeconds().toString().padStart(2, '0')
+		document.title = date.getHours().toString().padStart(2,'0') + ":"
+		+ date.getMinutes().toString().padStart(2,'0') + ":"
+		+ date.getSeconds().toString().padStart(2, '0')
     }
     function start(){
         setTime();
